29c652ef4681552a8c9c27859e54f1c15c89c784
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-01 21:13:34 -0600

    Delete all scans

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_reader/models/scan_model.dart';
 import 'package:flutter_qr_reader/pages/addresses_page.dart';
 import 'package:flutter_qr_reader/pages/history_page.dart';
-import 'package:flutter_qr_reader/services/db_provider.dart';
 import 'package:flutter_qr_reader/services/scan_provider.dart';
 import 'package:flutter_qr_reader/services/ui_provider.dart';
 import 'package:flutter_qr_reader/widgets/custom_navigationbar.dart';
@@ -14,13 +12,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scanProvider = Provider.of<ScanProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: const Text('Historial'),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.delete_forever))
+          IconButton(
+              onPressed: () {
+                scanProvider.deleteAllScans();
+              },
+              icon: const Icon(Icons.delete_forever))
         ],
       ),
       body: const _HomePageBody(),
