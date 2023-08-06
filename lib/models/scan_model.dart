181be224eeb53faed0c1fdf4d7fcf7f52bd77e4c
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-05 20:40:39 -0600

    Go to marker implementation

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class ScanModel {
   int? id;
   String? tipo;
@@ -19,6 +21,14 @@ class ScanModel {
     } else {
       tipo = 'geo';
     }
+  }
+
+  LatLng getLatLng() {
+    final latlng = this.valor.substring(4).split(',');
+    final lat = double.parse(latlng[0]);
+    final lng = double.parse(latlng[1]);
+
+    return LatLng(lat, lng);
   }
 
   factory ScanModel.fromJson(String str) => ScanModel.fromMap(json.decode(str));
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -15,11 +15,6 @@ class _MapPageState extends State<MapPage> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
@@ -28,25 +23,46 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Mapa'), centerTitle: true),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
+    final ScanModel scan =
+        ModalRoute.of(context)!.settings.arguments as ScanModel;
+
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 17.5,
+      tilt: 50,
     );
+
+    Set<Marker> markers = Set<Marker>();
+    markers.add(Marker(
+        markerId: const MarkerId('geo-loc'), position: scan.getLatLng()));
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Mapa'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () async {
+                  final newCam = CameraPosition(
+                      target: scan.getLatLng(), zoom: 17.5, tilt: 60);
+                  await _goToTheMarker(newCam);
+                },
+                icon: const Icon(Icons.location_searching_outlined))
+          ],
+        ),
+        body: GoogleMap(
+          myLocationButtonEnabled: false,
+          markers: markers,
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToTheMarker(CameraPosition marker) async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(marker));
   }
 }
